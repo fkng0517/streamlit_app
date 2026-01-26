85dbda5ceec7e24b22f1723097e7215263c3ac05
fkng0517 <t125013@ed.sus.ac.jp>
2026-01-26 11:34:25 +0900

    グラフの実装と判定を追加

--- app.py
+++ app.py
@@ -44,7 +44,59 @@ with tab1:
         delta_val = int(pref_data['24年件数']) - int(pref_data['23年件数'])
         # delta_color="inverse" をつけると「増えると赤」になる
         c3.metric("前年比", f"{delta_val} 件", delta=delta_val, delta_color="inverse")
-        
+
         st.divider()
 
+        # 棒グラフ
+        st.subheader('1. 年度別の推移')
+        
+        # Matplotlibを使ってグラフを描く
+        fig, ax = plt.subplots(figsize=(6, 4))
+        ax.bar(['2023', '2024'], [pref_data['23年件数'], pref_data['24年件数']], color=['lightgray', 'khaki'])
+        
+        # ここで単位を追加！
+        ax.set_ylabel('Cases') 
+        ax.set_title('Year')
+        
+        st.pyplot(fig)
+
+    # 増減率折れ線グラフ
+    st.subheader('2. 選択した地域の増減比較')
+
+    pref_map = {
+    '札幌方面': 'Sapporo', '函館方面': 'Hakodate', '旭川方面': 'Asahikawa', '釧路方面': 'Kushiro', '北見方面': 'Kitami', '青森県': 'Aomori', '岩手県': 'Iwate', '宮城県': 'Miyagi', '秋田県': 'Akita', '山形県': 'Yamagata', '福島県': 'Fukushima',
+    '東京都': 'Tokyo', '茨城県': 'Ibaraki', '栃木県': 'Tochigi', '群馬県': 'Gunma', '埼玉県': 'Saitama', '千葉県': 'Chiba', '神奈川県': 'Kanagawa', '新潟県': 'Nigata', '山梨県': 'Yamanashi', '長野県': 'Nagano',
+    '静岡県': 'Shizuoka', '富山県': 'Toyama', '石川県': 'Ishikawa', '福井県': 'Fukui',  '岐阜県': 'Gifu', '愛知県': 'Aichi', '三重県': 'Mie',
+    '滋賀県': 'Shiga', '京都府': 'Kyoto', '大阪府': 'Osaka', '兵庫県': 'Hyogo', '奈良県': 'Nara', '和歌山県': 'Wakayama',
+    '鳥取県': 'Tottori', '島根県': 'Shimane', '岡山県': 'Okayama', '広島県': 'Hiroshima', '山口県': 'Yamaguchi',
+    '徳島県': 'Tokushima', '香川県': 'Kagawa', '愛媛県': 'Ehime', '高知県': 'Kochi',
+    '福岡県': 'Fukuoka', '佐賀県': 'Saga', '長崎県': 'Nagasaki', '熊本県': 'Kumamoto', '大分県': 'Oita', '宮崎県': 'Miyazaki', '鹿児島県': 'Kagoshima', '沖縄県': 'Okinawa',
+    }
+    
+    filtered_df['Pref_En'] = filtered_df['都道府県'].map(pref_map)
+    fig2, ax2 = plt.subplots(figsize=(8, 4))
+    ax2.plot(filtered_df['Pref_En'], filtered_df['増減率'], marker='o', color='blue')
+    
+    ax2.set_ylabel('Change in cases')
+    ax2.set_xlabel('Prefecture')
+    plt.xticks(rotation=45) 
+    ax2.grid(True, axis='y', linestyle='--') 
+    
+    st.pyplot(fig2)
+
+with tab2:
+    st.header('警戒レベル判定')
+    for pref in selected_pref:
+        p_data = df[(df['区分'] == selected_crime) & (df['都道府県'] == pref)].iloc[0]
+        change = int(p_data['増減率'])
+
+        with st.expander(f"{pref}の判定結果"):
+            if change > 0:
+                st.error(f"【警戒】{selected_crime}が前年より {change} 件増加しています。")
+                st.write("夜間の外出や戸締まりに十分注意してください。")
+            elif change < 0:
+                st.success(f"【良好】{selected_crime}が前年より {-change} 件減少しています。")
+                st.write("治安は改善傾向にありますが、防犯意識は維持しましょう。")
+            else:
+                st.info(f"【維持】{selected_crime}の発生数は昨年と変わりません。")
 
